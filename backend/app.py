9c369164d7a8dc49895ffd3b564ad8c232e71c49
Sanha Lim <sl475@duke.edu>
2020-12-05 12:45:47 -0600

    seperated out db and made sub functions

--- backend/app.py
+++ backend/app.py
@@ -1,59 +1,48 @@
 from flask import Flask, jsonify, abort, make_response
 from flask_restful import Api, Resource, reqparse, fields, marshal
-from flask_sqlalchemy import SQLAlchemy
 import sqlalchemy.orm
 from cockroachdb.sqlalchemy import run_transaction
-from datetime import datetime
+from backend.models import CheckIn, db
 
 app = Flask(__name__, static_url_path="")
 api = Api(app)
+db.init_app(app)
 
 app.config.from_pyfile('ketchup.cfg')
-db = SQLAlchemy(app)
 sessionmaker = sqlalchemy.orm.sessionmaker(db.engine)
-
-class CheckIn(db.Model):
-    #example model from tutorial
-    __tablename__ = 'daily_checkin'
-    id = db.Column('checkin_id', db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    text = db.Column(db.String)
-    sentiment = db.Column(db.Float)
-    emotion = db.Column(db.String)
-    date = db.Column(db.DateTime)
-
-    def __init__(self, user_id, text, sentiment, emotion="None"):
-        self.user_id = user_id
-        self.text = text
-        self.sentiment = sentiment
-        self.emotion = emotion
-        self.date = datetime.utcnow()
-
-
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    children = db.relationship("CheckIn")
-
-    def __init__(self, name):
-        self.name = name
 
 class EmotionTranslater(Resource):
     def __init__(self):
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument('response', type=str, required=True,
+        self.reqparse.add_argument('text', type=str, required=True,
                                    help='No response provided',
                                    location='json')
         super(EmotionTranslater, self).__init__()
 
+    def add_checkin_to_db(self, ketchup):
+        def callback(session):
+            session.add(ketchup)
+        run_transaction(sessionmaker, callback)
+
+    def checkin_by_user(self, user_id):
+        def callback(session):
+            return session.query(CheckIn).order_by(CheckIn.date.desc()).limit(10).all()
+        run_transaction(sessionmaker, callback)
+
     def get(self, id):
-        #make a database call to cockroach db to
-        return {}
+        ketchup_bottle = self.checkin_by_user(id)
+        #do some sort of analysis
+        return {"most_freq_emotion": "", "average_sentiment": 0}
 
     def post(self, id):
         args = self.reqparse.parse_args()
-        return {}, 201
+        text = args["text"]
+        #send text to google cloud translate api
+        sentiment = 0
+        emotion = ""
+        ketchup = CheckIn(id, text, sentiment, emotion)
+        self.add_checkin_to_db(ketchup)
+        return {"emotion": emotion, "sentiment": sentiment}, 201
 
 api.add_resource(EmotionTranslater, '/api/emotion/<int:id>', endpoint='tasks')
 
